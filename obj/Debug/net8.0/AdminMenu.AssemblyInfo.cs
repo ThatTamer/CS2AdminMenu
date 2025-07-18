67c10ec2fec22c3f5fd99a5666088659bb97b8bd
ThatTamer <CadenKline8@gmail.com>
2025-07-18 03:20:53 -0400

    Initial commit

--- obj/Debug/net8.0/AdminMenu.AssemblyInfo.cs
+++ obj/Debug/net8.0/AdminMenu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdminMenu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+714457c2c657f74e4ca2ff5191a704bd7be2bb41")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdminMenu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdminMenu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
